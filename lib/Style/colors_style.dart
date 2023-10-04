e2c1b0edf6a70bcf405a018f4f01ab85176f167e
Adnif <ahmadbani9@gmail.com>
2023-10-04 16:27:45 +0700

    third work

--- lib/Style/colors_style.dart
+++ lib/Style/colors_style.dart
@@ -3,3 +3,4 @@ import 'dart:ui';
 //55, 57, 63
 const primary = Color.fromARGB(255, 87, 101, 242);
 const secondary = Color.fromARGB(255, 55, 57, 63);
+const bodyc = Color.fromARGB(255, 185, 187, 190);
