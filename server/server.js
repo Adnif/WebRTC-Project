f1520af9550a27a2998858d269490e0e9c3ad4e4
Adnif <ahmadbani9@gmail.com>
2023-10-10 10:56:57 +0700

    init server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Route Registry
+
+
+//Create the server
+const server = http.createServer(app);
+
+
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        server.listen(PORT, () => {
+            console.log(`Server is listening on ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("database connection failed. Server not started");
+        console.error(err);
+    })
+
